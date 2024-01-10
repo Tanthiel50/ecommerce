106945e2a5b0a9e0da18dbcddae9915e6863ffa3
Tanthiel50 <66637911+Tanthiel50@users.noreply.github.com>
2024-01-10 15:37:54 +0100

    fixtures user

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+// src/DataFixtures/UserFixtures.php
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // Créez et persistez les objets User ici
+        $user = new User();
+        $user->setEmail('user@user.fr');
+        $user->setFirstName('User');
+        $user->setLastName('User');
+        $user->setPseudo('user');
+        $user->setAdress('1 rue de la rue');
+        $user->setCity('Paris');
+        $user->setZipCode('75000');
+        $user->setCreatedAt(new \DateTime());
+        $user->setPassword('$argon2id$v=19$m=65536', 'user');
+        $user->setRoles(['ROLE_USER']);
+
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('admin@admin.fr');
+        $user->setFirstName('Admin');
+        $user->setLastName('Admin');
+        $user->setPseudo('admin');
+        $user->setAdress('1 rue de la rue');
+        $user->setCity('Paris');
+        $user->setZipCode('75000');
+        $user->setCreatedAt(new \DateTime());
+        $user->setPassword('$argon2id$v=19$m=65536', 'admin');
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
